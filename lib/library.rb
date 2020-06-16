a5672cfe4a08010345de1ebb0172bc8836bbab5f
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-16 11:33:26 +0000

    User Story 3 complete

--- lib/library.rb
+++ lib/library.rb
@@ -1,2 +1,6 @@
+require_relative 'book'
 class Library
+
+def add(book)
+end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,5 +1,6 @@
 require 'library'
 
+
 describe Library do
   describe '#initialize' do
     it 'expects library to be an instance of Library' do
@@ -7,4 +8,10 @@ describe Library do
     expect(library).to be_an_instance_of(Library)
     end
   end
+  
+describe '#add' do
+  it 'expects to respond to add method with 1 argument' do
+    expect(subject).to respond_to(:add).with(1).argument
+  end
+end
 end
